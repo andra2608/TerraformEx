70a1cb9d4591f4d814af01f8d513be7f931dfa9d
johithkrishna <johithkrishna0110@gmail.com>
2024-08-02 14:21:04 +0530

    reconfig

--- main.tf
+++ main.tf
@@ -60,23 +60,23 @@ resource "aws_security_group" "public_sg" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-  from_port = 22
-  to_port = 22
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress = {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
-  egress = {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -95,10 +95,10 @@ resource "aws_security_group" "private_sg" {
   cidr_blocks = ["10.0.0.0/16"]
   }
  
-  egress = {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
